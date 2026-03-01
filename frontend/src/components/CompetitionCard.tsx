a9121e7e0109e889deb654c63c05b4b24057e6a6
artizard <nicholascc76@gmail.com>
2026-02-28 21:42:10 -0500

    Changed how competitions are marked as official

--- frontend/src/components/CompetitionCard.tsx
+++ frontend/src/components/CompetitionCard.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import {Competition} from "@/types/Competition";
 
 interface CompetitionProps {
@@ -13,13 +14,21 @@ export default function CompetitionCard({ competition, update = false } : Compet
     return (
 
         <div className={"flex flex-col items-center justify-center w-full"}>
-            {competition.official ? (
-                <div className={"text-white text-sm"}>Official WCA Competition</div>) : null}
             <Link href={update ? `/admin/update/${slug}` : `/competitions/${slug}`} className={"w-full"}>
                 <div className={"flex w-full"} key={competition.id}>
                     <div className={"bg-orange-300 border border-4 border-r-0 border-black " +
-                        "w-full p-5 text-xl text-center text-black"}>
-                        {competition.name}
+                        "w-full p-5 text-xl text-center text-black flex items-center justify-center relative"}>
+                        {competition.official ? (
+                            <div className="absolute left-5">
+                                <Image
+                                    src={"/wca-icon.svg"}
+                                    height={45}
+                                    width={45}
+                                    alt="Official WCA Competition"
+                                />
+                            </div>
+                        ) : null}
+                        <span className="text-center">{competition.name}</span>
                     </div>
                     <div className={
                         "flex items-center text-center text-black bg-orange-300 p-5 " +
