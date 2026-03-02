e759ff7e726217d8f4cd0a46123369ef27b1b681
artizard <nicholascc76@gmail.com>
2026-03-02 02:42:45 -0500

    Added ability to add url from admin create competition

--- backend/competitions/serializers.py
+++ backend/competitions/serializers.py
@@ -162,6 +162,7 @@ class CompetitionCreateSerializer(serializers.Serializer):
     name = serializers.CharField()
     location = serializers.CharField()
     official = serializers.BooleanField()
+    official_link = serializers.URLField(required=False, allow_null=True)
     start_time = serializers.DateTimeField()
     end_time = serializers.DateTimeField()
 
