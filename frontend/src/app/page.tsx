d1cbc7c5b5ff0aa2844cb0fd05d6dcd894661adb
artizard <nicholascc76@gmail.com>
2026-03-12 22:31:44 -0400

    added wip custom background

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import dynamic from 'next/dynamic';
-
-
+import TextScramble from '@/components/TextScramble';
 
 const ModelViewer = dynamic(() =>
     import('@/components/CubeScene'), { 
@@ -10,13 +9,19 @@ const ModelViewer = dynamic(() =>
         <div className="w-11 h-11 border-5 border-white/30 border-t-white rounded-full animate-spin" />
       </div>});
 
+const ParticleBackground = dynamic(() =>
+    import('@/components/HomeBackground'), {ssr: false});
+
 export default function CubePage() {
     return (
-        <div className={""}>
-            <div className="flex flex-col  items-center pt-[15vh]">
+        <div className="relative min-h-screen overflow-hidden">
+            <ParticleBackground />
+
+            <div className="relative z-10 flex flex-col items-center pt-[15vh]">
                 <div className={"text-center"}>
-                    {/*TODO: i want to add a fun scramble/mix animation for the title words when first loaded*/}
-                    <h1 className="text-6xl text-white mb-15">Cubing Club at VT</h1>
+                    <h1 className="text-6xl text-white mb-15">
+                        <TextScramble text="Cubing Club at VT"/>
+                    </h1>
                 </div>
                 <ModelViewer />
             </div>
